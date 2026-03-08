928409574c5264bc7b08075803de6e8fe275f2f6
xRumax <167364@student.uwm.edu.pl>
2026-03-08 20:10:00 +0100

    feat: Finish admin panel for Car

--- app/fleet/admin.py
+++ app/fleet/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models.car import Car
 
-admin.site.register(Car)
+@admin.register(Car)
+class AdminCar(admin.ModelAdmin):
+    list_display = ("brand", "model", "year", "hp", "engine_type")
+
+    list_filter = ('brand', 'model')
+
+    search_fields = ('brand', 'model')
+
+    ordering = ('brand', 'model')
