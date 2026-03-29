254a45b12ff3ac906fbd6aa6021fb640f2b471d4
xRumax <167364@student.uwm.edu.pl>
2026-03-29 20:05:50 +0200

    feat(rentals): add overlapping dates validation to Rental model

--- app/rentals/models/rental.py
+++ app/rentals/models/rental.py
@@ -1,9 +1,8 @@
 from django.db import models
-from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.validators import MinValueValidator
 from django.core.exceptions import ValidationError
 from django.conf import settings
-import datetime
-from rentals.services import rental_service
+from rentals.services.rental_service import get_max_date, check_car_availability, check_start_date
 
 class Rental(models.Model):
     RENT_CHOICES = [
@@ -15,16 +14,8 @@ class Rental(models.Model):
     car = models.ForeignKey("fleet.car", on_delete = models.PROTECT, related_name='rentals')
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.PROTECT, related_name = 'rentals')
 
-    start_date = models.DateField(
-        validators=[
-            MinValueValidator(datetime.date.today),
-        ]
-    )
-    end_date = models.DateField(
-        validators=[
-            MinValueValidator(datetime.date.today),
-            MaxValueValidator(rental_service.get_max_date())]
-    )
+    start_date = models.DateField()
+    end_date = models.DateField()
     actual_return_date = models.DateTimeField(null= True, blank = True)
 
     total_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, validators=[
@@ -35,9 +26,29 @@ class Rental(models.Model):
     end_mileage = models.PositiveIntegerField(null = True, blank = True)
 
     def clean(self):
+        super().clean()
+
+        check_start_date(self.start_date)
+
+        if not self.start_date or not self.end_date:
+            return
+
         # End date vs Start date
-        if self.start_date and self.end_date and self.end_date < self.start_date:
-            raise ValidationError({"end_date": "End date must be after start date"})
+        if self.end_date < self.start_date:
+            raise ValidationError({'end_date':'End date must be after start date'})
+
+        max_allowed_date = get_max_date(self.start_date)
+
+        if self.end_date > max_allowed_date:
+            raise ValidationError({'end_date':"Hire is allowed for only 5 years, not more"})
+        
+        # Overlapping
+        check_car_availability(self.car, self.start_date, self.end_date, current_rental_id= self.id)
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
+
 
     def __str__(self):
         return f"{self.user} - {self.car} ({self.start_date})"
--- app/rentals/services/rental_service.py
+++ app/rentals/services/rental_service.py
@@ -1,4 +1,27 @@
 import datetime
+from django.core.exceptions import ValidationError
 
-def get_max_date():
-    return datetime.date.today()+ datetime.timedelta(days=365*5)
+def get_max_date(start_date):
+    if not start_date:
+        return datetime.date.today() + datetime.timedelta(days = 365*5)
+    
+    return start_date + datetime.timedelta(days = 365*5)
+
+def check_start_date(start_date):
+    if start_date < datetime.date.today():
+        raise ValidationError("Start date cannot be in the past")
+    
+def check_car_availability(car, start_date, end_date, current_rental_id = None):
+    from rentals.models.rental import Rental
+
+    overlapping_rentals = Rental.objects.filter(
+        car = car,
+        start_date__lt = end_date,
+        end_date__gt = start_date,
+    ).exclude(status = 'cancelled')
+
+    if current_rental_id:
+        overlapping_rentals = overlapping_rentals.exclude(id = current_rental_id)
+    
+    if overlapping_rentals.exists():
+        raise ValidationError("This car is already hired.")
