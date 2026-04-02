98198f629ce035685cabeedba2b9ab59208f4fe8
xRumax <167364@student.uwm.edu.pl>
2026-04-02 22:45:37 +0200

    Feat(JWT): Complete Register endpoint for user using jwt and confirm password

--- app/accounts/serializers/user_serializer.py
+++ app/accounts/serializers/user_serializer.py
@@ -1,28 +1,36 @@
 from rest_framework import serializers
 from accounts.models.user import User
 from django.core.exceptions import ValidationError as DjangoValidationError
+from django.contrib.auth.password_validation import validate_password
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'password']
+        fields = ['id', 'username', 'email',]
+
+class RegisterUserSerializer(serializers.ModelSerializer):
+    password_confirm = serializers.CharField(write_only = True)
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'password_confirm',]
         extra_kwargs = {
-            'password' : {'write_only': True}
+            'password': {'write_only':True},
+            'email':{'required':True}
         }
 
-    def create(self, validated_data):
-        user = User.objects.create_user(
-            username = validated_data['username'],
-            password = validated_data['password'],
-        )
-        return user
-    
-
     def validate(self,data):
-        try: 
-            instance = User(**data)
-            instance.full_clean()
-        except DjangoValidationError as e:
-            raise serializers.ValidationError(e.message_dict)
+        if data['password'] != data['password_confirm']:
+            raise serializers.ValidationError({'password': 'Passwords do not match.'})
+        
+        validate_password(data['password'])
+
         return data
     
+    def create(self, validated_data):
+        validated_data.pop('password_confirm')
+
+        user = User.objects.create_user(**validated_data)
+        return user
--- app/accounts/views/views.py
+++ app/accounts/views/views.py
@@ -1,9 +1,31 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
 from accounts.models.user import User
-from accounts.serializers.user_serializer import UserSerializer
+from accounts.serializers.user_serializer import UserSerializer, RegisterUserSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated]
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return RegisterUserSerializer
+        return UserSerializer
+
+    def get_permissions(self):
+        if self.action == 'create':
+            return [AllowAny()]
+        if self.action == 'list':
+            return [IsAdminUser()]
+        return [IsAuthenticated()]
+    
+    @action(detail=False, methods=['get'], permission_classes=[IsAuthenticated])
+    def me(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
+    
+
+
+
+    
