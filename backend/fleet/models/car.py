6d8c21f3dca07348f5cebd541133c1c31c2dcfe7
xRumax <ruminski.dawid77@gmail.com>
2026-04-22 22:54:39 +0200

    feat: add mileage field to Car model and update price field

--- backend/fleet/models/car.py
+++ backend/fleet/models/car.py
@@ -24,8 +24,8 @@ class Car(models.Model):
     number_plate = models.CharField(max_length = 10, unique = True, verbose_name="Number Plate")
     vin = models.CharField(max_length = 17, unique = True, verbose_name="VIN Number")
     image = models.ImageField(upload_to='cars/', null=True, blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)], null=True, blank=True)
-    mileage = models.DecimalField(max_digits= 10, decimal_palces =2, validators=[MinValueValidator(0)])
+    price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)], null=True, blank=True, default = 150)
+    mileage = models.DecimalField(max_digits= 10, decimal_places =2, validators=[MinValueValidator(0)], null=True, blank = True)
     
     def __str__(self):
         return f"{self.brand} {self.model} ({self.year})"
