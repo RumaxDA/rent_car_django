bb78aaa9f493ace570ddf7dea9e830e169ab06ea
xRumax <ruminski.dawid77@gmail.com>
2026-05-19 20:33:09 +0200

    udpate: Add urls for swagger documentation Redoc

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import (
     SpectacularAPIView,
-    SpectacularSwaggerView,
+    SpectacularSwaggerView, SpectacularRedocView
 ) 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -35,6 +35,7 @@ urlpatterns = [
     path('api/refresh/', TokenRefreshView.as_view(), name = "token_refresh"),
     path("api/schema/",SpectacularAPIView.as_view(), name = "schema"),
     path("api/docs/",SpectacularSwaggerView.as_view(url_name ="schema"), name = "swagger-ui"),
+    path("api/redoc/",SpectacularRedocView.as_view(url_name ="schema"), name = "redoc"),
     path("api/", include(fleet_router.urls)),
     path("api/", include(users_router.urls)),
     path("api/", include(rentals_router.urls)),
