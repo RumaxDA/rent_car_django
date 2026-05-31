1e8ca0beb198740b6f237b8d12eeacd92f4b5120
xRumax <ruminski.dawid77@gmail.com>
2026-05-31 23:01:27 +0200

    refactor: flatten project structure, remove obsolete backend dir

--- backend/rentals/urls.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views.rental_views import RentalViewSet
-
-router = DefaultRouter()
-router.register("rentals", RentalViewSet)
-
-urlpatterns = router.urls
---
+++ fleet/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models.car import Car
+
+
+@admin.register(Car)
+class AdminCar(admin.ModelAdmin):
+    list_display = ("brand", "model", "year", "hp", "engine_type")
+
+    list_filter = ("brand", "model")
+
+    search_fields = ("brand", "model")
+
+    ordering = ("brand", "model")
