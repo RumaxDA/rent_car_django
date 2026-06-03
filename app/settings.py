90b98f115ad2d9497999ca0b336d32c2f1022e4a
xRumax <ruminski.dawid77@gmail.com>
2026-06-03 19:47:00 +0200

    update: slight improvements

--- app/settings.py
+++ app/settings.py
@@ -166,7 +166,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": True,
     "BACKLIST_AFTER_ROTATION": True,
